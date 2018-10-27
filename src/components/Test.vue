e25e042402761b7d12b74afbb8080e890774cb01
mtdhrk <matsuda.ddcm@gmail.com>
2018-10-27 17:51:52 +0900

    設定いじり・テストコンポーネント追加

---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>
+    {{ msg }}
+  </p>
+</template>
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: '実験ページ'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
